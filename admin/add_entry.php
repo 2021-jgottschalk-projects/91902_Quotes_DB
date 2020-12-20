35ec481253c48592d2d10cc1db242a38b049735c
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-12-20 14:30:40 +1300

    admin works for existing authors

--- admin/add_entry.php
+++ admin/add_entry.php
@@ -1,5 +1,44 @@
 <?php
 
+// function to get subject ID
+function get_subjectID ($dbconnect, $subject)
+{
+    // get subject ID's if they exist...
+    $subid_sql = "SELECT * FROM `subject` WHERE `Subject` LIKE '$subject'";
+    $subid_query = mysqli_query($dbconnect, $subid_sql);
+    $subid_rs = mysqli_fetch_assoc($subid_query);
+    $subid_count=mysqli_num_rows($subid_query);
+    
+    
+    // if subject ID does not exist, add new subject to database
+    
+    if($subid_count > 0) {
+        $subject_ID = $subid_rs['Subject_ID'];
+        
+        echo "Subject ID".$subject_ID;
+        
+        return $subject_ID;
+    }
+    
+    else {
+        $add_subject_sql ="INSERT INTO `subject` (`Subject_ID`, `Subject`) VALUES (NULL, '$subject');";
+        $add_subject_query = mysqli_query($dbconnect, $add_subject_sql);
+        
+        // get subject ID
+        
+        $new_sub_sql = "SELECT * FROM `subject` WHERE `Subject` LIKE '$subject'";
+        $new_sub_query = mysqli_query($dbconnect, $new_sub_sql);
+        $new_sub_rs = mysqli_fetch_assoc($new_sub_query);
+        
+        $subject_ID = $new_sub_rs['Subject_ID'];
+        
+        echo "Subject ID".$subject_ID;
+        
+        return $subject_ID;
+        
+    }
+}
+
 $author_ID = $_SESSION['Add_Quote'];
 echo $author_ID;
 
@@ -46,36 +85,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // put checking code here in due course...
     
+    $subjectID_1 = get_subjectID($dbconnect, $tag_1);
+    $subjectID_2 = get_subjectID($dbconnect, $tag_2);
+    $subjectID_3 = get_subjectID($dbconnect, $tag_3);
     
-    
-    // get subject ID's if they exist...
-    $subid_sql = "SELECT * FROM `subject` WHERE `Subject` LIKE '$tag_1'";
-    $subid_query = mysqli_query($dbconnect, $subid_sql);
-    $subid_rs = mysqli_fetch_assoc($subid_query);
-    $subid_count=mysqli_num_rows($subid_query);
-    
-    
-    // if subject ID does not exist, add new subject to database
-    if($subid_count > 0) {
-        $tag_1_ID = $subid_rs['Subject_ID'];
-    }
-    
-    else {
-        $add_subject_sql ="INSERT INTO `subject` (`Subject_ID`, `Subject`) VALUES (NULL, '$tag_1');";
-        $add_subject_query = mysqli_query($dbconnect, $add_subject_sql);
-        
-        // get subject ID
-        
-        $new_sub_sql = "SELECT * FROM `subject` WHERE `Subject` LIKE '$tag_1'";
-        $new_sub_query = ($dbconnect, $new_sub_sql);
-        $new_sub_rs = mysqli_fetch_assoc($new_sub_query);
-        
-        $tag_1_ID = $new_sub_rs['Subject_ID'];
-        
-    }
+    echo "tag 1: ".$subjectID_1."<br />";
+    echo "tag 2: ".$subjectID_2."<br />";
+    echo "tag 3: ".$subjectID_3."<br />";
     
     // add entry to database
-    
+    $addentry_sql = "INSERT INTO `quotes` (`ID`, `Author_ID`, `Quote`, `Notes`, `Subject1_ID`, `Subject2_ID`, `Subject3_ID`) VALUES (NULL, '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3');";
+    $addentry_query = mysqli_query($dbconnect, $addentry_sql);
     
     // get quote ID for next page
     
